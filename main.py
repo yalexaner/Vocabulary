f7d65f63ccb2946b4f485e1fb7aaee8ee2fcd994
Alexander Yachmenev <yachmenev.aleksandr2000@gmail.com>
2017-01-15 20:37:08 +0500

    Init commit

---
+++ main.py
@@ -0,0 +1,80 @@
+import os
+
+def word_in(word, lst):
+	start = 0
+	end = len(lst) - 1
+	mid = end // 2
+
+	if end == -1:
+		return (False, -1)
+	
+	while end - start > 1:
+		if word < lst[mid]:
+			end = mid - 1
+			mid = (mid - start) // 2 + start
+		elif word > lst[mid]:
+			start = mid + 1
+			mid = (end - mid) // 2 + mid
+		else:
+			return (True, -1)
+
+	if word < lst[start]:
+		return (False, start)
+	elif lst[start] < word < lst[end]:
+		return (False, end)
+	elif lst[start] <= lst[end] < word:
+		return (False, end + 1)
+	else:
+		return (True, -1)
+
+vocabulary = []
+
+# reading vocabulary
+if os.path.exists("vocabulary.txt"):
+	with open("vocabulary.txt") as read:
+		for word in read:
+			vocabulary.append(word.strip())
+
+# choice
+choice = int(input("Add words - 1\nGet words - 2\n"))
+
+if choice == 1:
+	path = str(input("Write file path and name: "))
+
+	# adding words
+	with open(path) as read:
+		for word in read:
+			word_in_vocabulary, index = word_in(word.strip(), vocabulary)
+
+			if not word_in_vocabulary:
+				vocabulary.insert(index, word.strip())
+
+	# output
+	with open("vocabulary.txt", 'w') as out:
+		for word in vocabulary:
+			out.write(word + '\n')
+else:
+	path = str(input("Write file path and name: "))
+	output_path = str(input("Write file-output path and name: "))
+	words = []
+
+	with open(path) as read:
+		for line in read:
+			for word in line.split():
+				if word.istitle() and len(word) > 1:
+					word = word.lower()
+
+				while not word.isalpha():
+					word = word[:-1]
+
+				if len(word) != 0:
+					word_was_added, index = word_in(word, words)
+					word_in_vocabulary, nothing = word_in(word, vocabulary)
+
+				if not word_was_added and not word_in_vocabulary:
+					words.append(word)
+
+	# output
+	with open(output_path, 'w') as out:
+		for word in words:
+			out.write(word + '\n')
